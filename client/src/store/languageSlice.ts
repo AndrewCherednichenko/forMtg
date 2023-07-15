b3a60976c58d812516cb43715ccfd9570fd8fb58
Andrew Cherednichenko <49998279+AndrewCherednichenko@users.noreply.github.com>
2023-07-15 11:59:55 +0300

    v1

--- client/src/store/languageSlice.ts
+++ client/src/store/languageSlice.ts
@@ -1,5 +1,5 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'; 
 
 type LanguageState = {
   language: string;
